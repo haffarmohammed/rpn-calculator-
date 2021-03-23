eadf2fb4c1ae35418102586da38a6e5bf8bb41da
HAMIDI Oussama <oussama.hamidi@ens.uvsq.fr>
2021-03-23 13:28:31 +0100

    quit test passed

--- src/test/java/fr/uvsq/poo/compte/CommandTest.java
+++ src/test/java/fr/uvsq/poo/compte/CommandTest.java
@@ -4,25 +4,21 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Scanner;
-
-import static org.junit.Assert.*;
 
 public class CommandTest {
 
     @Test
     public void testUndo(){
-        Calculator calculator = new Calculator();
-        InputStream sysInBackup = System.in; // backup System.in to restore it later
-        ByteArrayInputStream in = new ByteArrayInputStream("quit".getBytes());
-        System.setIn(in);
-        calculator.session();
 
     }
 
     @Test
     public void testQuit(){
-
+        Calculator calculator = new Calculator();
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("quit".getBytes());
+        System.setIn(in);
+        calculator.session();
     }
 
 }
