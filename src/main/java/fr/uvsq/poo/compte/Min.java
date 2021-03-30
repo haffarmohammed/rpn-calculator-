166103fb64126a9c77052864eb968ef25d50284d
HAMIDI Oussama <oussama.hamidi@ens.uvsq.fr>
2021-03-30 14:22:58 +0200

    Undo minus test passed

--- src/main/java/fr/uvsq/poo/compte/Min.java
+++ src/main/java/fr/uvsq/poo/compte/Min.java
@@ -9,13 +9,19 @@ public class Min extends Command{
 
     @Override
     public void execute() {
-        int op = interpreter.operands.pop();
-        interpreter.operands.push( interpreter.operands.pop() - op);
+        this.operand = interpreter.operands.pop();
+        interpreter.operands.push( interpreter.operands.pop() - this.operand);
+        interpreter.history.push(this);
         interpreter.afficher();
     }
 
     @Override
     public void undo() {
-
+        int dif = interpreter.operands.pop();
+        System.out.println(dif);
+        System.out.println(this.operand);
+        interpreter.operands.push(dif + this.operand);
+        interpreter.operands.push(this.operand);
+        interpreter.history.pop();
     }
 }
--- src/test/java/fr/uvsq/poo/compte/CommandTest.java
+++ src/test/java/fr/uvsq/poo/compte/CommandTest.java
@@ -66,4 +66,11 @@ public class CommandTest {
         moteurRPN.session();
     }
 
+    @Test
+    public void testUndoMinusNumbers(){
+        ByteArrayInputStream in = new ByteArrayInputStream("5\n6\n-\nundo\nundo\nundo\nquit".getBytes());
+        System.setIn(in);
+        moteurRPN.session();
+    }
+
 }
